3050e0083f4c846c3afdafdf7e1a369a1c95fdf0
Alexandra Pavlova <pavlovaalexandra29@gmail.com>
2022-01-20 15:47:39 +0300

    get text layer

--- djvu utils/get_text.py
+++ djvu utils/get_text.py
@@ -2,28 +2,30 @@ from tqdm.notebook import tqdm
 
 import djvu.decode
 
-def print_text(sexpr, level=0):
+def print_text(sexpr, text_file, level=0):
     if level > 0:
-        print(' ' * (2 * level - 1), end=' ')
+        text_file.write(' ' * (2 * level - 1) + ' ')
     if isinstance(sexpr, djvu.sexpr.ListExpression):
         if len(sexpr) == 0:
             return
-        print(str(sexpr[0].value), [sexpr[i].value for i in range(1, 5)])
+        text_file.write(str(sexpr[0].value))
+        text_file.write(str([sexpr[i].value for i in range(1, 5)]) + "\n")
         for child in sexpr[5:]:
-            print_text(child, level + 1)
+            print_text(child, text_file, level + 1)
     else:
-        print(sexpr)
+        text_file.write(str(sexpr) + "\n")
 
 class Context(djvu.decode.Context):
 
-    def process(self, djvu_path, pages=[]):
+    def process(self, djvu_path, text_file, pages=[]):
         document = self.new_document(djvu.decode.FileURI(djvu_path))
         for i, page in tqdm(enumerate(document.pages)):
             page.get_info(wait=True)
             if i not in pages and pages != []:
                 continue
-            print_text(page.text.sexpr)
+            print_text(page.text.sexpr, text_file)
 
-def dump_text(djvu_path, pages=[]):
+def dump_text(djvu_path, text_file_path, pages=[]):
+    text_file = open(text_file_path, 'w')
     context = Context()
-    context.process(djvu_path, pages)
+    context.process(djvu_path, text_file, pages)
