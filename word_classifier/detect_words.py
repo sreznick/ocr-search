5d32d04833013e5c7760f89cb8d3d150b0a18a06
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-05-16 23:28:32 +0300

    Separate module for MSER detection

--- word_classifier/detect_words.py
+++ word_classifier/detect_words.py
@@ -1,38 +1,30 @@
 import os
 import numpy as np
 import cv2
+import shared_paths as paths
+from word_detectors import mser
 
 
-DATA_DIR = os.path.join(os.getcwd(), 'data')
-
-# testing -- process single page
-INPUT_FILE = os.path.join(DATA_DIR, 'pages', '02', '21.png')
-INTERIM_FILE = os.path.join(DATA_DIR, 'tmp.png')
-OUTPUT_FILE = os.path.join(DATA_DIR, 'out.png')
+INPUT_IMAGE = os.path.join(paths.PAGE_DIR, '07', '71.png')
 MAX_WIDTH = 800
+INTERIM_IMAGE = os.path.join(paths.DATA_DIR, 'tmp.png')
+OUTPUT_IMAGE = os.path.join(paths.DATA_DIR, 'out.png')
 
 
 def main():
-    img = cv2.imread(INPUT_FILE)
-    img2 = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, w = img.shape
+    img = cv2.imread(INPUT_IMAGE)
+    _, w, _ = img.shape
     k = min(1, MAX_WIDTH / w)
     img = cv2.resize(img, None, fx=k, fy=k)
-    img2 = cv2.resize(img2, None, fx=k, fy=k)
-    # img = cv2.blur(img, (1, 5))
-    _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
-    kernel = np.ones((1, 7), dtype='uint8')
-    img = cv2.dilate(img, kernel, iterations=1)
-    cv2.imwrite(INTERIM_FILE, img)
-
-    mser = cv2.MSER.create(min_area=150, max_area=10000)
-    _, bboxes = mser.detectRegions(img)
-    print(len(bboxes))
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img2 = mser.preprocess(img2, (100, 200),
+                           dilate_kernel=np.ones((1, 7), dtype='uint8'))
+    cv2.imwrite(INTERIM_IMAGE, img2)
+    bboxes = mser.mser_detector(img2, min_area=100, max_area=10000)
     for bbox in bboxes:
         x, y, w, h = bbox
-        cv2.rectangle(img2, (x, y), (x+w, y+h), (0, 255, 0), 1)
-    cv2.imwrite(OUTPUT_FILE, img2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
+    cv2.imwrite(OUTPUT_IMAGE, img)
 
 
 if __name__ == '__main__':
